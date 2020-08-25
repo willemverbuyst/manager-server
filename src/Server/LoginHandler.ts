f3732fb162886f3078bd2a00a53900ca5dbba878
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-25 15:44:18 +0200

    Add interfaces

--- src/Server/LoginHandler.ts
+++ src/Server/LoginHandler.ts
@@ -1,6 +1,7 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import { Account, Handler } from './Model';
 
-export class LoginHandler {
+export class LoginHandler implements Handler {
   private req: IncomingMessage;
   private res: ServerResponse;
 
@@ -16,7 +17,7 @@ export class LoginHandler {
     console.log('request password: ', body.password);
   }
 
-  private async getRequestBody(): Promise<any> {
+  private async getRequestBody(): Promise<Account> {
     return new Promise((resolve, reject) => {
       let body = '';
       this.req.on('data', (data: string) => {
