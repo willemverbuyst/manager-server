88c7e69248dc390714ae824ac8e5368158dc65dd
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-25 13:22:54 +0200

    Use private and pubkic modifiers

--- Launcher.ts
+++ Launcher.ts
@@ -1,14 +1,14 @@
 import { Server } from './Server';
 
 class Launcher {
-  name: string;
-  server: Server;
+  private name: string;
+  private server: Server;
 
   constructor() {
     this.server = new Server();
   }
 
-  launchApp() {
+  public launchApp() {
     console.log('The app started');
     this.server.createServer();
   }
--- Server.ts
+++ Server.ts
@@ -1,5 +1,5 @@
 export class Server {
-  createServer() {
+  public createServer() {
     console.log('Created server');
   }
 }
