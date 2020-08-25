4d5ca3d61ff277fbb99a376f8095c6837f3fdf2a
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-25 13:17:26 +0200

    Set up project with launcher and server

---
+++ Server.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.Server = void 0;
+var Server = /** @class */ (function () {
+    function Server() {
+    }
+    Server.prototype.createServer = function () {
+        console.log('Created server');
+    };
+    return Server;
+}());
+exports.Server = Server;
