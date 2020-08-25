baeddb6cbb621e6cca2eab457b37246a186217cb
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-25 13:55:20 +0200

    Create server

--- src/Server/Server.ts
+++ src/Server/Server.ts
@@ -1,5 +1,11 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
 export class Server {
   public createServer() {
-    console.log('Created server');
+    createServer((req: IncomingMessage, res: ServerResponse) => {
+      console.log('got request from: ' + req.url);
+      res.end();
+    }).listen(8080);
+    console.log('serverStarted');
   }
 }
