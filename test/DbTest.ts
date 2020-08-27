19875aabc60f3fe3a8c0804277d2708d6d96725e
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-27 10:49:28 +0200

    Create db for users

--- test/DbTest.ts
+++ test/DbTest.ts
@@ -1,11 +1,22 @@
 import { UserCredentialsDBAccess } from '../src/Authorization/UserCredentialsDBAccess';
+import { UsersDBAccess } from '../src/User/UsersDBAccess';
 
 class DbTest {
   public dbAcess: UserCredentialsDBAccess = new UserCredentialsDBAccess();
+
+  public userDBAccess: UsersDBAccess = new UsersDBAccess();
 }
 
-new DbTest().dbAcess.putUserCredential({
-  username: 'user1',
-  password: '1234',
-  accessRights: [1, 2, 3],
+// new DbTest().dbAcess.putUserCredential({
+//   username: 'user1',
+//   password: '1234',
+//   accessRights: [1, 2, 3],
+// });
+
+new DbTest().userDBAccess.putUser({
+  id: 'adfaferwrq24576',
+  name: 'John Doe',
+  age: 30,
+  email: 'some@email.com',
+  workingPosition: 3,
 });
