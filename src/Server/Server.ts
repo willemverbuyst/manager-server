9c7b7573f376eb4eb12754e41a15f46fa99c7d9e
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-30 09:06:06 +0200

    Adjust server for cors and OPTIONS

--- src/Server/Server.ts
+++ src/Server/Server.ts
@@ -11,6 +11,7 @@ export class Server {
     createServer(async (req: IncomingMessage, res: ServerResponse) => {
       console.log('got request from: ' + req.method);
       console.log('got request from: ' + req.url);
+      this.addCorsHeader(res);
       const basePath = Utils.getUrlBasePath(req.url);
 
       switch (basePath) {
@@ -27,5 +28,11 @@ export class Server {
       }
       res.end();
     }).listen(8080);
+    console.log('Server started');
+  }
+
+  private addCorsHeader(res: ServerResponse) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
   }
 }
